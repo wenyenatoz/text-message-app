80aa617493792d4e6ece7e569b4decfd05849353
wenyen atoz <wenyen.koh@atoz-software.tech>
2023-02-23 15:43:48 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react'
+import { PubSub } from 'pubsub-js';
+import {createWebSocket,closeWebSocket,websocket} from './websocket';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Ws(){
+    
+useEffect(() => {
+    let url="ws://192.168.56.1:8086/ppq/999";//服务端连接的url
+        createWebSocket(url)
+        let messageSocket=null;
+        messageSocket = PubSub.subscribe('message',getMsg)
+        //在组件卸载的时候，关闭连接
+         return ()=>{
+            PubSub.unsubscribe(messageSocket); 
+            closeWebSocket();
+        }
+}, []);
+
+const sendMsg=()=>{
+    let msg='发送消息'
+    websocket&&websocket.send(msg)
+    console.log('ws发送')
 }
 
-export default App;
+const getMsg=(topic,message)=>{
+    console.log('ws获取：',message)
+}
+
+const regInput = React.useRef();
+
+const reCreateWS=()=>{
+    const v = regInput.current.value;
+    if(v != null && v !==''){
+        closeWebSocket();
+        createWebSocket(v);
+    }
+}
+    return(
+        <div>
+            <input type="text" id='urlText' ref={regInput}/>
+            <button onClick={reCreateWS} >连接websocket</button>
+            <button onClick={sendMsg}>发送消息</button>
+            <button onClick={closeWebSocket}>关闭连接</button>
+        </div>
+    )
+}
+
+export default Ws
